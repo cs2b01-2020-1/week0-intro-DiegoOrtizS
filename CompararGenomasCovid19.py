3f507f08032e76e13b3d34a118b94e1d4e89ca5c
DiegoOrtizS <52261652+DiegoOrtizS@users.noreply.github.com>
2020-03-28 17:56:58 -0500

    Update CompararGenomasCovid19.py

--- CompararGenomasCovid19.py
+++ CompararGenomasCovid19.py
@@ -23,17 +23,8 @@ def CompararGenomasDeCoronavirus(archivo1, archivo2):
     cont  = 100*cont
     return cont
 
-def PalabrasPorArchivo(array):
+def Llenar(matriz, array):
     lista = [0]*len(array)
-    for i in range(len(array)):
-        archivoCopia = open(array[i], "r")
-        archivoCopia_ = str(archivoCopia.read())
-        lista[i] = len(archivoCopia_)
-        archivoCopia.close()
-    return lista
-
-def Llenar(array):
-    palabrasArchivo = PalabrasPorArchivo(archivos)
 
     print("Considerando las columnas como el conjunto de genomas de partida y las filas como el conjunto de genomas de llegada")
     print("Para calcular el porcentaje de similitud se ha multiplicado el número de similitudes por 100")
@@ -42,25 +33,29 @@ def Llenar(array):
 
     for i in range (len(archivos)):
         print("CovidGenoma",i+1, end = "    ")
+        archivoCopia = open(array[i], "r")
+        archivoCopia_ = str(archivoCopia.read())
+        lista[i] = len(archivoCopia_)
+        archivoCopia.close()
     print("")
 
     for i in range (len(archivos)):
         print("CovidGenoma",i+1, end = "       ")
         for j in range (len(archivos)):
             if j > i:
-                matriz[i][j] = CompararGenomasDeCoronavirus(archivos[j],archivos[i])
-                print(round(matriz[i][j]/palabrasArchivo[j],2), end = "            ")
+                matriz[i][j] = CompararGenomasDeCoronavirus(array[j],array[i])
+                print(round(matriz[i][j]/lista[j],2), end = "            ")
             else:
                 if i == j:
                     matriz[i][j] = "100  "
                 else:
-                    matriz[i][j] = round(matriz[j][i]/(palabrasArchivo[j]), 2)
+                    matriz[i][j] = round(matriz[j][i]/(lista[j]), 2)
                 print(matriz[i][j], end = "            ")
         print("")
 
 archivos = ["AY274119.txt", "AY278488.2.txt", "MN908947.txt", "MN988668.txt", "MN988669.txt"]
 
 matriz = [[0 for i in range(len(archivos))]for i in range(len(archivos))]
-Llenar(matriz)
+Llenar(matriz, archivos)
 tiempoFinal   = time()
 print("\nTiempo de ejecución: ", tiempoFinal - tiempoInicial)
